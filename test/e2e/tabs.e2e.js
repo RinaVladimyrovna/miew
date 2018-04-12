ce26d72a2284d9dc2f17623d5e6af4eec2d7cc26
Rina <efedorovaw@gmail.com>
2018-04-12 12:17:47 +0300

    additional comments for tab tests

--- test/e2e/tabs.e2e.js
+++ test/e2e/tabs.e2e.js
@@ -11,14 +11,20 @@ import _ from 'lodash';
 
 chai.use(dirtyChai);
 
+// make another report for tab content only
+// all separate reports should have different names
 const cfg = Object.assign({}, goldenCfg, {
   title: 'Tabs Tests',
   report: 'tabs-containment-tests.html',
 });
 
 let driver, page;
-
-// make something with those functions
+////////////////////////////////////////
+// make something with those functions//
+// there is no time for grammar =-/   //
+////////////////////////////////////////
+// creates an array of all descendants of a given html element by x-Path
+// need it to decrease number of locators listed in the static file
 function getDescenders(rootElement, xPath) {
   let descendants = [];
   return driver.findElement(rootElement).findElements(xPath)
@@ -27,22 +33,25 @@ function getDescenders(rootElement, xPath) {
       return descendants;
     });
 }
-
+// takes an array of html elements and creates an array of their text
+// in a way it should be represented to a user
 function getElementsText(elementsToIterate) {
   let textsArray = [];
   elementsToIterate.forEach((elem) => elem.getText().then((elemText) => textsArray.push(elemText)));
   return textsArray;
 }
-
+// just checks that an element is on the page
 function getHtmlElementVisiability(htmlElement) {
   return htmlElement.isDisplayed().then((result) => console.log(result));
 };
 /////////////////////////////////////////////////////////////
+// gets innerHTML of an element
 function takeElementsText(elementToTakeTextOf) {
   let singleElementText = elementToTakeTextOf.getAttribute('innerText');
   return singleElementText;
 }
-
+// creates an array of all first descendants of an element
+// it can be merged with "getDescenders(rootElement, xPath)" swiftly
 function findElementsFirstDescendants(elementToFindDescendantsOf) {
   return elementToFindDescendantsOf.findElements(By.xpath('./*'))
     .then((firstStageDescendants) => {
@@ -51,7 +60,9 @@ function findElementsFirstDescendants(elementToFindDescendantsOf) {
       return descendants;
     });
 }
-
+// creates an array of elements' text
+// yes, it is a dublicate now
+// realisation is different
 function takeEveryDescentsText(elementToOperateOn) {
   return findElementsFirstDescendants(elementToOperateOn)
     .then((firstStateDescendants) => {
@@ -63,7 +74,14 @@ function takeEveryDescentsText(elementToOperateOn) {
       return descendantsText;
     });
 }
-
+// listen carefully my friend
+// the point is selenium can take text of an element including
+// text of all descendants only (other ways gets errors in some browsers
+// and we want our tests to run in many browsers the same way, do not we?
+// Since we want to get a text of an element only we have to get text of
+// all its descendants and get rid of descendant text pattern in parent
+// element text. So we get a long text (parent + descendants), get
+// descendant text only and replace the latter with "" in the former.
 function replaceOriginalElementsTextParts(originalElement) {
   let originalElementText;
   return takeElementsText(originalElement)
@@ -78,7 +96,7 @@ function replaceOriginalElementsTextParts(originalElement) {
       return originalElementText;
     });
 }
-
+// Here we gather all parent texts together.
 function getAllDescentElementsUniqueText(elementsArrayToIterateIn) {
   let resultString = [];
   elementsArrayToIterateIn.forEach((singleElement) => {
@@ -92,7 +110,7 @@ describe('As a power user, I want to check menu functionality, e. g.', function(
 
   this.timeout(0);
   this.slow(1000);
-
+// create a selenium instance
   before(function() {
     driver = createDriverInstance();
     return golden.startup(driver, cfg)
@@ -104,33 +122,34 @@ describe('As a power user, I want to check menu functionality, e. g.', function(
         golden.report.data.version = version;
       });
   });
-
+// terminate a selenium instance after all tests are run
   after(function() {
     return golden.shutdown();
   });
-
+// update a report every time
   beforeEach(function() {
     golden.report.context.desc = this.currentTest.title;
   });
+// wait till a default molecule is built
 ///////is it needed???
   it('see 1CRN by default', function() {
     return page.waitUntilTitleContains('1CRN')
       .then(() => page.waitUntilRebuildIsDone())
       .then(() => golden.shouldMatch('1crn', this));
   });
-
+// find a Menu button otherwise message
   it('have Menu button visible', function() {
-    return driver.wait(until.elementLocated(locator.openMenuButton), 5 * 1000)
+    return driver.wait(until.elementLocated(locator.openMenuButton), 5 * 1000, "there is no Menu button here")
       .then(() => driver.findElement(locator.openMenuButton).isDisplayed())
       .then((state) => expect(state).to.equal(true));
   });
-
+// combine selectors and names together
   let tabs = completeArray(locator.leftPanel.tabs.selector, locator.leftPanel.tabs.names);
 
   describe('have all tabs visible', function() {
 
     let suite = this;
-
+// check that every tab name is present on navigation panel after Menu button is clicked
     before(function() {
       tabs.forEach((tab) => {
         suite.addTest(it(`${tab.name} is displayed`, function() {
@@ -139,20 +158,22 @@ describe('As a power user, I want to check menu functionality, e. g.', function(
         }));
       });
     });
-
+// check the navigation panel is opened technically, it can be invisible though
     it('see Info panel after Menu is opened', function() {
       return driver.findElement(locator.openMenuButton).click()
-        .then(() => driver.wait(until.elementLocated(locator.leftPanel.css), 5 * 1000))
+        .then(() => driver.wait(until.elementLocated(locator.leftPanel.css), 5 * 1000), "Menu button was clicked but navigation panel is absent")
         .then(() => driver.findElement(tabs[0].css).getAttribute('class'))
         .then((attribute) => expect(attribute.split(/\s/).slice(-1)[0]).to.equal('active'));
     });
 
   });
-
+// see description
   describe('check Info panel containment', function() {
 
     let suite = this;
-
+// the result was an array first but now it is a string and array coparison is commented
+// check text of every descendant from the list (see static file)
+// it is better to check every element text separately but now we compare it by groups
     before(function() {
       _.each(locator.infoPanel.body.descendant.xPath, (xPath, index) => {
         suite.addTest(it(`get Info panel body descendants text ${index}`, function() {
@@ -169,7 +190,7 @@ describe('As a power user, I want to check menu functionality, e. g.', function(
         //}));
       });
     });
-
+// check total text from the panel
     it('Info panel total text', function() {
       return driver.findElement(locator.infoPanel.panel.css).findElements(By.xpath('.//*'))
         .then((foundElements) => getAllDescentElementsUniqueText(foundElements))
@@ -181,7 +202,7 @@ describe('As a power user, I want to check menu functionality, e. g.', function(
     });
 
   });
-
+// and repeat test above for every tab
   describe('check Load panel containment', function() {
 
     let suite = this;
@@ -199,7 +220,7 @@ describe('As a power user, I want to check menu functionality, e. g.', function(
         }));
       });
     });
-
+// pay attention to the fact the Info tab is opened by default, but other tabs should be opened by this function
     it('open Load panel', function() {
       return driver.findElement(tabs[1].css).click()
         .then(() => driver.findElement(tabs[1].css).getAttribute('class'))
